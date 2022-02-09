69bc229bddf5cab207b7f9f0fba27313c878a0f1
SalieriC <salieric@t-online.de>
2022-02-09 09:41:39 +0100

    change shaken functional

--- succ/scripts/init.js
+++ succ/scripts/init.js
@@ -1,3 +1,13 @@
 Hooks.on(`ready`, () => {
     console.log('SWADE Ultimate Condition Changer | Ready');
+    change_conditions();
 });
+
+async function change_conditions() {
+    //First, change all the default condition images:
+    CONFIG.statusEffects.find(st => st.id === 'shaken').icon = "modules/succ/assets/icons/0-Shaken.png"
+}
+
+async function add_conditions() {
+    //Add custom conditions:
+}
