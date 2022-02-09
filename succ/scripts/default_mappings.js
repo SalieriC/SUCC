6b69dec81b0453ceb22aedc7b553c6c00a24c9b4
javier <javier@castroparga.com>
2022-02-09 17:20:58 +0100

    Use json for icon mapping.

---
+++ succ/scripts/default_mappings.js
@@ -0,0 +1,5 @@
+// Default mappings for icons
+
+export const SUCC_DEFAULT_MAPPING = {
+    'shaken': 'modules/succ/assets/icons/0-Shaken.png'
+}
--- succ/scripts/init.js
+++ succ/scripts/init.js
@@ -1,11 +1,19 @@
+// Init script
+/* globals Hooks, console, CONFIG */
+
+import {SUCC_DEFAULT_MAPPING} from "./default_mappings.js";
+
 Hooks.on(`ready`, () => {
     console.log('SWADE Ultimate Condition Changer | Ready');
     change_conditions();
 });
 
-async function change_conditions() {
-    //First, change all the default condition images:
-    CONFIG.statusEffects.find(st => st.id === 'shaken').icon = "modules/succ/assets/icons/0-Shaken.png"
+function change_conditions() {
+    for (let status of CONFIG.statusEffects) {
+        if (status.id in SUCC_DEFAULT_MAPPING) {
+            status.icon = SUCC_DEFAULT_MAPPING[status.id]
+        }
+    }
 }
 
 async function add_conditions() {
