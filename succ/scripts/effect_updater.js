dcaecc5693b891aa3a5ecc8eeea24948e0db50bb
SalieriC <salieric@t-online.de>
2022-02-24 17:10:55 +0100

    loca for protection builder

--- succ/scripts/effect_updater.js
+++ succ/scripts/effect_updater.js
@@ -6,7 +6,9 @@ export async function effect_updater(condition, userID) {
     }
     console.log(actorOrToken.data.effects)
     if (condition.data.flags?.core?.statusId === "smite" && game.user.id === userID) {
-        //Smite stuff
+        const appliedCondition = actorOrToken.data.effects.find(function (e) {
+            return ((e.data.label === game.i18n.localize("SWADE.Smite")))
+        })
     } else if (condition.data.flags?.core?.statusId === "protection" && game.user.id === userID) {
         const appliedCondition = actorOrToken.data.effects.find(function (e) {
             return ((e.data.label === game.i18n.localize("SWADE.Protection")))
@@ -14,12 +16,12 @@ export async function effect_updater(condition, userID) {
         console.log(appliedCondition)
         //Protection stuff
         new Dialog({
-            title: "Protection builder",
+            title: game.i18n.localize("SUCC.dialogue.protection_builder_name"),
             content: `
-            <h2><img src=${condition.data.icon} width="30" height="30" style="border:0;" /> Protection Builder</h2>
-            <p><italic>Protection</italic> increases the armor or toughness of the recipient. Please choose the amount to increase and click on the appropriate button, depending on modifiers used by the spellcaster.</p>
+            <h2><img src=${condition.data.icon} width="30" height="30" style="border:0;" /> ${game.i18n.localize("SUCC.dialogue.protection_builder_name")}</h2>
+            ${game.i18n.localize("SUCC.dialogue.protection_builder_dialogue")}
             <div style="display:flex">
-                <p style="flex:3">Amount to increase: </p>
+                <p style="flex:3">${game.i18n.localize("SUCC.dialogue.amount_to_increase")}</p>
                 <input type="number" id="protectionAmount" value="2" style="flex:1"/></input>
             </div>
             `,
