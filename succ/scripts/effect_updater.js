2bf61e6156579023bf4f475ce3afd5149c6fb5c8
SalieriC <salieric@t-online.de>
2022-02-25 00:43:15 +0100

    smite builder

--- succ/scripts/effect_updater.js
+++ succ/scripts/effect_updater.js
@@ -4,16 +4,59 @@ export async function effect_updater(condition, userID) {
         // Use the token if unlinked:
         actorOrToken = condition.parent.parent
     }
-    console.log(actorOrToken.data.effects)
+    //console.log(actorOrToken.data.effects)
     if (condition.data.flags?.core?.statusId === "smite" && game.user.id === userID) {
         const appliedCondition = actorOrToken.data.effects.find(function (e) {
             return ((e.data.label === game.i18n.localize("SWADE.Smite")))
         })
+        
+        const weapons = actorOrToken.data.items.filter(i => i.type === "weapon")
+        if (weapons.length === 0) {return}
+        let weapOptions
+        for (let weapon of weapons) {
+            weapOptions = weapOptions + `<option value="${weapon.data.name}">${weapon.data.name}</option>`
+        }
+
+        new Dialog({
+            title: game.i18n.localize("SUCC.dialogue.smite_builder_name"),
+            content: `
+            <h2><img src=${condition.data.icon} width="30" height="30" style="border:0;" /> ${game.i18n.localize("SUCC.dialogue.smite_builder_name")}</h2>
+            ${game.i18n.localize("SUCC.dialogue.smite_builder_dialogue")}
+            <div style="display:flex">
+                <p style="flex:3">${game.i18n.localize("SUCC.dialogue.weapon_to_affect")}</p>
+                <select id="weapon">${weapOptions}</select>
+            </div>
+            <div style="display:flex">
+                <p style="flex:3">${game.i18n.localize("SUCC.dialogue.amount_to_increase")}</p>
+                <input type="number" id="damageBonus" value="2" style="flex:1"/></input>
+            </div>
+            `,
+            buttons: {
+                apply: {
+                    label: game.i18n.localize("SUCC.dialogue.apply"),
+                    callback: async (html) => {
+                        let selectedWeaponName = html.find(`#weapon`)[0].value
+                        let damageBonus = Number(html.find("#damageBonus")[0].value);
+                        if (damageBonus >= 0) { damageBonus = '+' + damageBonus }
+
+                        appliedCondition.setFlag('swade', 'expiration', 3);
+                        let updates = appliedCondition.toObject() //foundry rejects identical objects -> You need to toObject() the effect then change the result of that then pass that over; it looses .data in the middle because toObject() is just the cleaned up data
+                        let change = {key: `@Weapon{${selectedWeaponName}}[data.actions.dmgMod]`, mode: 2, priority: undefined, value: damageBonus}
+                        updates.changes = [change]
+                        updates.duration.rounds = 5
+                        await appliedCondition.update(updates)
+                    }
+                },
+                cancel: {
+                    label: game.i18n.localize("SUCC.dialogue.cancel")
+                }
+            }
+        }).render(true)
     } else if (condition.data.flags?.core?.statusId === "protection" && game.user.id === userID) {
         const appliedCondition = actorOrToken.data.effects.find(function (e) {
             return ((e.data.label === game.i18n.localize("SWADE.Protection")))
         })
-        console.log(appliedCondition)
+        //console.log(appliedCondition)
         //Protection stuff
         new Dialog({
             title: game.i18n.localize("SUCC.dialogue.protection_builder_name"),
@@ -27,7 +70,7 @@ export async function effect_updater(condition, userID) {
             `,
             buttons: {
                 armor: {
-                    label: "Armor",
+                    label: game.i18n.localize("SWADE.Armor"),
                     callback: async (html) => {
                         let protectionAmount = Number(html.find("#protectionAmount")[0].value);
                         let updates = appliedCondition.toObject().changes //foundry rejects identical objects -> You need to toObject() the effect then change the result of that then pass that over; it looses .data in the middle because toObject() is just the cleaned up data
@@ -36,7 +79,7 @@ export async function effect_updater(condition, userID) {
                     }
                 },
                 toughness: {
-                    label: "Toughness",
+                    label: game.i18n.localize("SWADE.Tough"),
                     callback: async (html) => {
                         let protectionAmount = Number(html.find("#protectionAmount")[0].value);
                         let updates = appliedCondition.toObject().changes //foundry rejects identical objects -> You need to toObject() the effect then change the result of that then pass that over; it looses .data in the middle because toObject() is just the cleaned up data
@@ -45,7 +88,7 @@ export async function effect_updater(condition, userID) {
                     }
                 },
                 cancel: {
-                    label: "Cancel"
+                    label: game.i18n.localize("SUCC.dialogue.cancel")
                 }
             }
         }).render(true)
