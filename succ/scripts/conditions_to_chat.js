9e5fb99f191bcd49109005664701b2af82782ace
javier <javier@castroparga.com>
2022-10-17 16:16:36 +0200

    Remove uses of .data that were causing warnings.

--- succ/scripts/conditions_to_chat.js
+++ succ/scripts/conditions_to_chat.js
@@ -29,7 +29,7 @@ export async function output_to_chat(condition, removed, userID) {
             journalLink = SUCC_DEFAULT_SWPF_LINKS_NEW[condition.flags.core.statusId]
             hasReference = true
         }
-    } else if (game.modules.get("swade-core-rules")?.active && (game.modules.get("swade-core-rules").data.version === "1.0.6" || game.modules.get("swade-core-rules").data.version === "1.0.7") ) { //temporary solution for changed IDs in SWADE core 1.0.6
+    } else if (game.modules.get("swade-core-rules")?.active && (game.modules.get("swade-core-rules").version === "1.0.6" || game.modules.get("swade-core-rules").version === "1.0.7") ) { //temporary solution for changed IDs in SWADE core 1.0.6
         if (condition.flags?.core?.statusId in SUCC_DEFAULT_SWADE_LINKS_106) {
             journalLink = SUCC_DEFAULT_SWADE_LINKS_106[condition.flags.core.statusId]
             hasReference = true
